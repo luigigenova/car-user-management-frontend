43c3999325dc4c1e5e97ff7fbcd53de8d12cd1d9
Luigi <systemasjava@gmail.com>
2024-11-15 23:20:40 -0300

    Add untracked files before merge

---
+++ .angular/cache/17.3.11/vite/deps/@angular_material_button.js
@@ -0,0 +1,35 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-SM3DK6MK.js";
+import "./chunk-KV7GFRHP.js";
+import "./chunk-FQEM7BSC.js";
+import "./chunk-SF7Q7RME.js";
+import "./chunk-6UHCRHZ7.js";
+import "./chunk-SDQMWN4J.js";
+import "./chunk-DSK7TZNG.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
